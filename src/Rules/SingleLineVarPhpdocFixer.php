ee742ba862dad7614e63707a73341b7bd335f25e
Giovanni Lenoci <giovanni.lenoci@facile.it>
2019-05-03 15:39:12 +0200

    Adds .idea to .gitignore Adds SingleLineVarPhpdocFixer for compressing multiline phpdoc when it contains only @var Adds SingleLineVarPhpdocFixerTest

---
+++ src/Rules/SingleLineVarPhpdocFixer.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Facile\CodingStandards\Rules;
+
+use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+use SplFileInfo;
+
+class SingleLineVarPhpdocFixer extends AbstractFixer
+{
+    private const NAME = 'single_line_var_phpdoc';
+
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    public function getDefinition()
+    {
+        return new FixerDefinition('Phpdoc comments containing only @var must be on a single line', []);
+    }
+
+    public function isCandidate(Tokens $tokens)
+    {
+        return $tokens->isTokenKindFound(T_DOC_COMMENT);
+    }
+
+    protected function applyFix(SplFileInfo $file, Tokens $tokens):void
+    {
+        /** @var Token $token */
+        foreach ($tokens as $index => $token) {
+            if (! $token->isComment()) {
+                continue;
+            }
+
+            $comment = preg_replace_callback(
+                '/(\s*)\/\*[\s*]+@var (.*)[\s*]+\*\//',
+                function($matches) {
+                    return $matches[1].'/** @var '.trim($matches[2]).' */';
+                },
+                $token->getContent()
+            );
+
+            if ($comment) {
+                $token->setContent($comment);
+            }
+        }
+    }
+
+}
---
+++ tests/Rules/SingleLineVarPhpdocFixerTest.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+namespace Facile\CodingStandardsTest\Rules;
+
+use Facile\CodingStandards\Rules\SingleLineVarPhpdocFixer;
+use PhpCsFixer\Tests\Test\AbstractFixerTestCase;
+
+class SingleLineVarPhpdocFixerTest extends AbstractFixerTestCase
+{
+
+    /**
+     * @dataProvider provideFixCases
+     */
+    public function testFix(string $expected, string $input) : void {
+        $this->doTest($expected, $input);
+    }
+
+    protected function createFixer()
+    {
+        return new SingleLineVarPhpdocFixer();
+    }
+
+    public function provideFixCases(): \Generator
+    {
+
+        $input = <<<'PHP'
+        <?php
+        
+        namespace Project\TheNamespace;
+        
+        class TheClass {
+        
+            /** 
+             * @var string  
+             */
+            private $var;
+
+            /** 
+             * My super cool property
+             * @var string  
+             */
+            private $superCoolProperty;
+
+            public function getVar(): string 
+            {
+                return $this->var;
+            }
+        }
+PHP;
+
+        $expected = <<<'PHP'
+        <?php
+        
+        namespace Project\TheNamespace;
+        
+        class TheClass {
+        
+            /** @var string */
+            private $var;
+
+            /** 
+             * My super cool property
+             * @var string  
+             */
+            private $superCoolProperty;
+
+            public function getVar(): string 
+            {
+                return $this->var;
+            }
+        }
+PHP;
+
+        yield [$expected, $input];
+
+    }
+
+}
