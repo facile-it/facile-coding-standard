9bddf1ee88a3a61151bcef5e31d19823d2b5476f
Thomas Vargiu <tvargiu@gmail.com>
2020-07-09 16:15:15 +0200

    Fixed tests

--- tests/Framework/TestCase.php
+++ tests/Framework/TestCase.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
 
 if (! \trait_exists(ProphecyTrait::class)) {
-    \class_alias(ProphecyTrait::class, EmptyProphecyTrait::class);
+    \class_alias(EmptyProphecyTrait::class, ProphecyTrait::class);
 }
 
 class TestCase extends PHPUnitTestCase
