8d1f84a929811a4c816e56e034bc696a3e3aecf2
Alessandro Lai <alessandro.lai85@gmail.com>
2021-08-27 15:33:07 +0200

    Add some unit tests

--- tests/AutoloadPathProviderTest.php
+++ tests/AutoloadPathProviderTest.php
@@ -62,4 +62,38 @@ class AutoloadPathProviderTest extends TestCase
         $expected = ['src/'];
         $this->assertSame($expected, $provider->getPaths());
     }
+
+    public function testGetPathsWithWrongComposerJsonPath(): void
+    {
+        $provider = new AutoloadPathProvider(__DIR__ . '/composer.json');
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectErrorMessage('Unable to find composer.json');
+
+        $provider->getPaths();
+    }
+
+    public function testGetPathsWithInvalidComposerJson(): void
+    {
+        $provider = new AutoloadPathProvider(
+            $this->composerFilePath,
+            $this->projectRoot,
+            false
+        );
+
+        file_put_contents($this->composerFilePath, '');
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectErrorMessage('Invalid composer.json file');
+
+        $provider->getPaths();
+    }
+
+    public function testWrongComposerPathLeadsToBrokenProjectPath(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectErrorMessage('Unable to get project root');
+
+        new AutoloadPathProvider('wrong/composer/path/');
+    }
 }
