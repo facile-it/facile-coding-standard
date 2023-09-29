1913885fe7e3731d5d2367e29007356819d3ab12
Alessandro Lai <alessandro.lai85@gmail.com>
2023-09-29 08:53:52 +0200

    Skip list test in prefer-lowest

--- tests/RulesMaintenance/DumperTest.php
+++ tests/RulesMaintenance/DumperTest.php
@@ -2,6 +2,7 @@
 
 namespace Facile\CodingStandardsTest\RulesMaintenance;
 
+use PhpCsFixer\Console\Application;
 use PHPUnit\Framework\TestCase;
 
 class DumperTest extends TestCase
@@ -22,10 +23,20 @@ class DumperTest extends TestCase
      */
     public function testNoRuleIsUnlisted(): void
     {
+        if ($this->isPreferLowest()) {
+            $this->markTestSkipped('This test is not reliable with older PHP-CS-Fixer version, due to new rules being added, or included in sets');
+        }
+        
         $dumper = new Dumper();
         $unlistedFixerNames = array_keys(iterator_to_array($dumper->getUnlistedRulesDescription()));
         sort($unlistedFixerNames);
 
         $this->assertEmpty($unlistedFixerNames, implode(\PHP_EOL, ['There are some unlisted rules:', ...$unlistedFixerNames]));
+    }
+
+    private function isPreferLowest(): bool
+    {
+        /** @psalm-suppress InternalClass */
+        return version_compare(Application::VERSION, '3.30.0') < 0;
     }
 }
