ad40814b251e23b1dc0481554d136736eb25a595
Alessandro Lai <alessandro.lai85@gmail.com>
2026-05-26 15:09:40 +0200

    Add Rector

--- rector.php
+++ rector.php
@@ -11,6 +11,7 @@ return RectorConfig::configure()
         __DIR__ . '/tests',
     ])
     ->withPhpSets()
+    ->withComposerBased()
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
